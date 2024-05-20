110235663a2752e6b82a0ad518b72de486909494
Elians Rocha <eliansro2001@gmail.com>
2024-05-20 15:15:04 -0500

    Inicialización del proyecto
    Inicialización del proyecto con todas las dependencias y artefactos.

---
+++ api-comunideport/src/main/java/com/comunideport/api/apicomunideport/ApiComunideportApplication.java
@@ -0,0 +1,13 @@
+package com.comunideport.api.apicomunideport;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiComunideportApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiComunideportApplication.class, args);
+	}
+
+}
---
+++ api-comunideport/src/test/java/com/comunideport/api/apicomunideport/ApiComunideportApplicationTests.java
@@ -0,0 +1,13 @@
+package com.comunideport.api.apicomunideport;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiComunideportApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
